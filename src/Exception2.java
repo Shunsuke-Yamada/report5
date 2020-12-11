6f95396bfba861972729c7c9380266aa818d2153
Shunsuke Yamada <e205719@ie.u-ryukyu.ac.jp>
2020-12-11 23:48:55 +0900

    report5 3rd commit

---
+++ src/Exception2.java
@@ -0,0 +1,7 @@
+public class step3 {
+    public static void main(String[] ags){
+        String str = "百二十三";
+        int vilue = Integer.parseInt(str);
+        System.out.println(vilue);
+    }
+}
